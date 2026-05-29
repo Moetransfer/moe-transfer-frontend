25e54d513df3029a126e41fcf435dbc91d3ae818
user <user@Mac.fritz.box>
2026-05-30 00:03:05 +0200

    fix activity blank screen

--- src/App.js
+++ src/App.js
@@ -211,7 +211,7 @@ function App() {
     🏠 Home
   </button>
 
-  <button className="nav-btn" onClick={() => alert("Activity page coming soon")}>
+  <button className="nav-btn" onClick={() => setScreen("home")}>
     ⇄ Activity
   </button>
 
